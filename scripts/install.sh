fb30914a64225395aa516ab96b2b2e08b1a711d4
Hugo Araya Nash <kabroxiko@gmail.com>
2026-07-23 21:35:57 -0400

    Initial commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -288,6 +288,10 @@ install_dayzctl() {
         error "Downloaded file not found: $TMP_FILE"
     fi
 
+
+
+
+
     ACTUAL_CHECKSUM=$(sha256sum "$TMP_FILE" | awk '{print $1}')
     log "Actual checksum: $ACTUAL_CHECKSUM"
 
