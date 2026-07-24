78b8ee432f7986804d0669f95f8a2f270156a79a
Hugo Araya Nash <kabroxiko@gmail.com>
2026-07-24 10:49:01 -0400

    Initial commit

--- cmd/dayzctl/main.go
+++ cmd/dayzctl/main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"os"
+
+	"github.com/kabroxiko/dayzctl/cmd/dayzctl/commands"
+)
+
 func main() {
-	// Start the urfave/cli application (migrated from Cobra)
-	RunApp()
+	// If invoked as `dayzctl rcon ...` run the urfave app (positional UX),
+	// otherwise run the legacy Cobra entrypoint so existing commands keep working.
+	if len(os.Args) > 1 && os.Args[1] == "rcon" {
+		RunApp()
+		return
+	}
+	commands.Execute()
 }
