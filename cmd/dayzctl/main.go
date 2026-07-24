a4ae6538f6b434db250c20fdf6a8c64af6bcd7a9
Hugo Araya Nash <kabroxiko@gmail.com>
2026-07-24 10:30:36 -0400

    Initial commit

--- cmd/dayzctl/main.go
+++ cmd/dayzctl/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/kabroxiko/dayzctl/cmd/dayzctl/commands"
+
 func main() {
-	RunApp()
+	// Use Cobra's Execute() to ensure persistent flags and PersistentPreRunE
+	// are parsed/executed (loads config, initializes logger, etc.).
+	commands.Execute()
 }
